855114fcc7103e86401c9d365772e217da12cfac
sneha <snehathakker2@gmail.com>
2017-12-26 20:20:54 +0530

    PHPCS and PHPCBF

--- construct.php
+++ construct.php
@@ -1,20 +1,19 @@
 <?php
-	class result
-	{
-		private $subject="ionic";
-		
-		public function __construct($subject=null)
-		{
-			if($subject)
-			{
-				$this->subject=$subject;
-			}
-		}
-		public function getsubject()
-		{
-			return 'The subject is:' .$this->subject;
-		}
-	}
-	$r= new result();
-	echo $r->getsubject();
+class result
+{
+    private $subject="ionic";
+        
+    public function __construct($subject=null)
+    {
+        if($subject) {
+            $this->subject=$subject;
+        }
+    }
+    public function getsubject()
+    {
+        return 'The subject is:' .$this->subject;
+    }
+}
+    $r= new result();
+    echo $r->getsubject();
 ?>
--- methods_properties.php
+++ methods_properties.php
@@ -1,22 +1,22 @@
 <?php
-	class car
-	{
-		public $tank;
-		public function fill($float)
-		{
-			$this->tank += $float;
-			return $this;
-		}
-		public function ride($float)
-		{
-			$miles = $float;
-			$gallons = $miles/50;
-			$this-> tank -= ($gallons);
-			
-			return $this;
-		}
-	}
-	$bmw =new car();
-	$tank = $bmw-> fill(10) -> ride(40) -> tank;
-	echo "gallons left in the tank:".$tank;
+class car
+{
+    public $tank;
+    public function fill($float)
+    {
+        $this->tank += $float;
+        return $this;
+    }
+    public function ride($float)
+    {
+        $miles = $float;
+        $gallons = $miles/50;
+        $this-> tank -= ($gallons);
+            
+        return $this;
+    }
+}
+    $bmw =new car();
+    $tank = $bmw-> fill(10) -> ride(40) -> tank;
+    echo "gallons left in the tank:".$tank;
 ?>
